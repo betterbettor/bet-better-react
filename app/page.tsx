d0972d0070d3b3ec9375d415f5bb50323c5961a9
Louvre Chow <louvrecly@gmail.com>
2023-10-19 15:03:32 -0400

    feat/integrate-server: create Loading component and wrap fetched data with Suspense in the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+import { Suspense } from 'react';
+import Loading from './loading';
+
 async function checkServerHealth() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BET_BETTER_API}/matches`);
 
@@ -10,7 +13,12 @@ const Home = async () => {
   const data = await checkServerHealth();
   return (
     <main>
-      <h1>Server Health: {JSON.stringify(data)}</h1>
+      <h1>
+        <span>Matches Response: </span>
+        <Suspense fallback={<Loading />}>
+          <span>{JSON.stringify(data)}</span>
+        </Suspense>
+      </h1>
     </main>
   );
 };
