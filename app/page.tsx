2e340d0d4db6e52657d01ec6e79b8278e21a5951
Louvre Chow <louvrecly@gmail.com>
2023-10-21 12:17:16 -0400

    feat/init-ui: add u- prefix to the tailwindcss utility classes

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,27 @@ async function checkServerHealth() {
 const Home = async () => {
   const data = await checkServerHealth();
   return (
-    <main>
-      <h1>
+    <main className="u-py-7 u-px-9">
+      <h1 className="u-text-xl">BetBetter</h1>
+
+      <div className="u-py-5">
+        <input
+          type="text"
+          className="u-py-2.5 u-px-7 u-w-full u-bg-green-300 u-rounded-full"
+          placeholder="Search for a team"
+        />
+      </div>
+
+      <button className="u-block u-ml-auto u-mb-5 u-py-2.5 u-px-7 u-bg-green-900 u-rounded">
+        Expand All
+      </button>
+
+      <div className="u-bg-green-100 u-rounded-xl">
         <span>Matches Response: </span>
         <Suspense fallback={<Loading />}>
           <span>{JSON.stringify(data)}</span>
         </Suspense>
-      </h1>
+      </div>
     </main>
   );
 };
