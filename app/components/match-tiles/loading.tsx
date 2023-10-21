950770ed375d5ad593d67471482fe29a1ee4fac8
Louvre Chow <louvrecly@gmail.com>
2023-10-21 16:34:50 -0400

    feat/match-tiles: extract MatchTiles and MatchTile components from the Home page UI

---
+++ app/components/match-tiles/loading.tsx
@@ -0,0 +1,5 @@
+const Loading = () => {
+  return <span>Loading match data...</span>;
+};
+
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import { Suspense } from 'react';
-import Loading from './loading';
-import Image from 'next/image';
+import MatchTiles from '@/app/components/match-tiles';
 import MatchResponseData from '@/interfaces/response.interface';
 
 async function fetchAllMatches(): Promise<MatchResponseData> {
@@ -30,91 +28,7 @@ const Home = async () => {
         Expand All
       </button>
 
-      <div className="u-py-5 u-px-7 u-bg-green-100 u-rounded-xl u-flex u-flex-col u-gap-5">
-        <Suspense fallback={<Loading />}>
-          {!matches.length
-            ? 'No matches data found'
-            : matches.map((match) => (
-                <div
-                  key={match.id}
-                  className="u-py-3 u-px-7 u-bg-green-50 u-rounded-xl u-border u-text-slate-900 u-shadow"
-                >
-                  <p className="u-text-xs u-flex u-justify-between">
-                    <span>
-                      Start Date:{' '}
-                      {new Date(match.startTime).toLocaleDateString()}
-                      {new Date(match.startTime).toLocaleTimeString()}
-                    </span>
-                    <span>
-                      Last updated:{' '}
-                      {new Date(match.lastUpdated).toLocaleDateString()}
-                      {new Date(match.lastUpdated).toLocaleTimeString()}
-                    </span>
-                  </p>
-
-                  <div className="u-flex u-items-center u-justify-between u-gap-3">
-                    <div className="u-flex-1 u-flex u-items-stretch u-justify-between u-gap-3">
-                      <div className="u-flex-1 u-flex u-flex-col u-justify-between u-gap-3">
-                        <div className="u-text-sm u-text-center u-font-bold">
-                          Home
-                        </div>
-
-                        <div className="u-flex u-items-center u-gap-3">
-                          <Image
-                            src={match.home.logo}
-                            alt={match.home.name}
-                            width={30}
-                            height={30}
-                          />
-                          <h2 className="u-text-lg u-font-bold">
-                            {match.home.name}
-                          </h2>
-                        </div>
-
-                        <div className="u-text-3xl u-text-center">
-                          {match.odds[match.odds.length - 1].home}
-                        </div>
-                      </div>
-
-                      <div className="u-flex-1 u-flex u-flex-col u-justify-between u-gap-3">
-                        <div className="u-text-sm u-text-center u-font-bold">
-                          Away
-                        </div>
-
-                        <div className="u-flex u-items-center u-gap-3">
-                          <Image
-                            src={match.away.logo}
-                            alt={match.away.name}
-                            width={30}
-                            height={30}
-                          />
-                          <h2 className="u-text-lg u-font-bold">
-                            {match.away.name}
-                          </h2>
-                        </div>
-
-                        <div className="u-text-3xl u-text-center">
-                          {match.odds[match.odds.length - 1].away}
-                        </div>
-                      </div>
-
-                      <div className="u-flex-1 u-flex u-flex-col u-justify-between u-gap-3">
-                        <div className="u-text-sm u-text-center u-font-bold">
-                          Draw
-                        </div>
-
-                        <div className="u-text-3xl u-text-center">
-                          {match.odds[match.odds.length - 1].draw}
-                        </div>
-                      </div>
-                    </div>
-
-                    <button className="u-w-5 u-h-5">V</button>
-                  </div>
-                </div>
-              ))}
-        </Suspense>
-      </div>
+      <MatchTiles matches={matches} />
     </main>
   );
 };
