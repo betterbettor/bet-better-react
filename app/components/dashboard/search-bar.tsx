12db15ec9041364c2c600be80df59c75963bb609
Louvre Chow <louvrecly@gmail.com>
2023-10-23 12:38:59 -0400

    feat/search-bar_suggestions: implement suggestions logic with allTeamNames and suggestedTeamNames memoized states in the Dashboard

--- app/components/dashboard/search-bar.tsx
+++ app/components/dashboard/search-bar.tsx
@@ -1,12 +1,39 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
 interface SearchBarProps {
+  value: string;
+  suggestions?: string[];
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onSuggestionClicked?: (suggestion: string) => void;
   onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
 }
 
-const SearchBar = ({ onChange, onSubmit }: SearchBarProps) => {
+const SearchBar = ({
+  value = '',
+  suggestions = [],
+  onChange,
+  onSuggestionClicked,
+  onSubmit,
+}: SearchBarProps) => {
   const [showSuggestions, setShowSuggestions] = useState(false);
+  const [timer, setTimer] = useState<NodeJS.Timeout | null>(null);
+
+  const handleSuggestionClick = (suggestion: string) => () => {
+    onSuggestionClicked && onSuggestionClicked(suggestion);
+  };
+
+  const handleBlur = () => {
+    if (!!timer) clearTimeout(timer);
+
+    const newTimer = setTimeout(() => setShowSuggestions(false), 300);
+    setTimer(newTimer);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <form className="u-py-5 u-flex" onSubmit={onSubmit}>
@@ -15,14 +42,25 @@ const SearchBar = ({ onChange, onSubmit }: SearchBarProps) => {
           type="text"
           className="u-py-3 u-px-7 u-w-full u-bg-green-300 u-text-slate-900 u-rounded-l-full"
           placeholder="Search for a team"
+          value={value}
           onFocus={() => setShowSuggestions(true)}
-          onBlur={() => setShowSuggestions(false)}
+          onBlur={handleBlur}
           onChange={onChange}
         />
 
         {showSuggestions && (
           <div className="u-py-3 u-px-7 u-absolute u-top-full u-left-0 u-bg-slate-950/70 u-rounded-xl">
-            Auto suggestions
+            {!suggestions.length
+              ? 'No matching team'
+              : suggestions.map((suggestion) => (
+                  <p
+                    key={suggestion}
+                    className="u-opacity-70 u-cursor-pointer hover:u-opacity-90"
+                    onClick={handleSuggestionClick(suggestion)}
+                  >
+                    {suggestion}
+                  </p>
+                ))}
           </div>
         )}
       </div>
