556b38355fe6e4889f761cc365ba50aa2d4a30d4
Louvre Chow <louvrecly@gmail.com>
2023-10-23 12:34:44 -0400

    feat/search-bar_suggestions: create suggestions placeholder div with showSuggestions state in the SearchBar component

--- app/components/dashboard/search-bar.tsx
+++ app/components/dashboard/search-bar.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 interface SearchBarProps {
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -6,14 +6,26 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ onChange, onSubmit }: SearchBarProps) => {
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
   return (
     <form className="u-py-5 u-flex" onSubmit={onSubmit}>
-      <input
-        type="text"
-        className="u-py-3 u-px-7 u-flex-1 u-bg-green-300 u-text-slate-900 u-rounded-l-full"
-        placeholder="Search for a team"
-        onChange={onChange}
-      />
+      <div className="u-relative u-flex-1">
+        <input
+          type="text"
+          className="u-py-3 u-px-7 u-w-full u-bg-green-300 u-text-slate-900 u-rounded-l-full"
+          placeholder="Search for a team"
+          onFocus={() => setShowSuggestions(true)}
+          onBlur={() => setShowSuggestions(false)}
+          onChange={onChange}
+        />
+
+        {showSuggestions && (
+          <div className="u-py-3 u-px-7 u-absolute u-top-full u-left-0 u-bg-slate-950/70 u-rounded-xl">
+            Auto suggestions
+          </div>
+        )}
+      </div>
 
       <button
         className="u-py-3 u-px-7 u-bg-green-900 u-rounded-r-full"
