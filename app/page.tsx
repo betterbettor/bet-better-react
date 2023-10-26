660f3f747243c788b19793b471f8e7d353c45aa1
Louvre Chow <louvrecly@gmail.com>
2023-10-26 19:34:55 -0400

    feat/tile-style: add max-width to main

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const Home = async () => {
   const matches = await fetchAllMatches();
 
   return (
-    <main className="u-p-5 u-min-h-screen sm:u-py-7 sm:u-px-9">
+    <main className="u-mx-auto u-p-5 u-min-h-screen u-max-w-5xl sm:u-py-7 sm:u-px-9">
       <h1 className="u-mb-5 u-text-3xl u-text-yellow-400 u-font-bold sm:u-text-5xl sm:u-text-center">
         <span className="u-inline-block u-rotate-0 u-transition-transform hover:u-rotate-180">
           ⚽
