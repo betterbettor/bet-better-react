46f0ccca9a922594c477cf6cb67c9682499e6e7e
Louvre Chow <louvrecly@gmail.com>
2023-10-26 12:20:12 -0400

    feat/tile-style: fix key as prop error

--- app/chart/data-points.tsx
+++ app/chart/data-points.tsx
@@ -4,8 +4,8 @@ import { ChartDataProps } from '@/interfaces/ui.type';
 interface DataPointsProps<T>
   extends ChartDataProps<T>,
     SVGProps<SVGCircleElement> {
-      key?: string;
-    }
+  chartKey?: string;
+}
 
 const DataPoints = ({
   data,
@@ -14,14 +14,14 @@ const DataPoints = ({
   scaleX,
   scaleY,
   r = 2.5,
-  key = '',
+  chartKey = '',
   ...props
 }: DataPointsProps<Record<string, any>>) => {
   return (
     <g>
       {data.map((d) => (
         <circle
-          key={`point-${key}-${yKey}-${d[xKey]}`}
+          key={`point-${chartKey}-${yKey}-${d[xKey]}`}
           className="u-opacity-70 hover:u-opacity-90"
           cx={scaleX(d[xKey])}
           cy={scaleY(d[yKey])}
