bca9eff6d4aaa071f3b7aaac8c10e33bae63ee91
Louvre Chow <louvrecly@gmail.com>
2023-10-26 10:41:09 -0400

    feat/date-picker: remove commented out SearchBar from the Dashboard

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { ChangeEvent, FormEvent, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import MatchTiles from './match-tiles';
-import SearchBar from './search-bar';
 import DatePicker from './date-picker';
 import { MatchResponse } from '@/interfaces/match.interface';
 import { ExpandedMap, ExpandedMapStates } from '@/interfaces/ui.type';
@@ -27,41 +26,15 @@ const Dashboard = ({ matches }: DashboardProps) => {
     return states;
   }, [matches]);
 
-  // const allTeamNames = useMemo(() => {
-  //   const allTeamNamesSet = matches.reduce((teamsSet, match) => {
-  //     teamsSet.add(match.home.name);
-  //     teamsSet.add(match.away.name);
-  //     return teamsSet;
-  //   }, new Set<string>());
-
-  //   return Array.from(allTeamNamesSet).sort();
-  // }, [matches]);
-
   const [expandedMap, setExpandedMap] = useState(
     expandedMapStates.ALL_COLLAPSED,
   );
 
-  // const [keyword, setKeyword] = useState('');
-  // const [query, setQuery] = useState('');
   const [startDate, setStartDate] = useState<Date>(new Date());
-
-  // const suggestedTeamNames = useMemo(() => {
-  //   return allTeamNames.filter((teamName) =>
-  //     teamName.toLowerCase().includes(keyword.toLowerCase().trim()),
-  //   );
-  // }, [allTeamNames, keyword]);
 
   const hasAllTilesExpanded = useMemo(() => {
     return matches.every((match) => expandedMap.get(match.id));
   }, [expandedMap, matches]);
-
-  // const filteredMatches = useMemo(() => {
-  //   return matches.filter(
-  //     (match) =>
-  //       match.home.name.toLowerCase().includes(query) ||
-  //       match.away.name.toLowerCase().includes(query),
-  //   );
-  // }, [matches, query]);
 
   const matchesFilteredByDate = useMemo(() => {
     return matches.filter(
@@ -69,18 +42,6 @@ const Dashboard = ({ matches }: DashboardProps) => {
         new Date(match.startTime).toDateString() === startDate.toDateString(),
     );
   }, [matches, startDate]);
-
-  // const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-  //   setKeyword(event.target.value);
-  // };
-
-  // const handleSuggestionClicked = (suggestion: string) => () =>
-  //   setKeyword(suggestion);
-
-  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   setQuery(keyword.toLowerCase().trim());
-  // };
 
   const handleToggleAll = () => {
     setExpandedMap(
@@ -110,15 +71,17 @@ const Dashboard = ({ matches }: DashboardProps) => {
             {leagueName}
           </div>
         </div>
+
         <div>
           <DatePicker startDate={startDate} onChange={handleChangeDate} />
         </div>
+
         <button
-          className={`u-block sm:u-ml-auto u-py-3 u-px-7 u-w-40 ${
+          className={`u-block sm:u-ml-auto u-py-3 u-px-7 u-w-40 u-rounded ${
             hasAllTilesExpanded
               ? 'u-bg-green-600'
               : 'u-bg-green-300 u-text-black'
-          } u-rounded`}
+          }`}
           onClick={handleToggleAll}
         >
           {hasAllTilesExpanded ? 'Collapse' : 'Expand'} All
