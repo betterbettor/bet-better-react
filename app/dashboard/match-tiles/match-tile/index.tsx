bbec26fb2ef5112f439370df2249da5cfb27e341
Louvre Chow <louvrecly@gmail.com>
2023-10-26 09:45:27 -0400

    feat/chart_refactor_DEBUG: replace odds with DUMMY_ODDS

--- app/dashboard/match-tiles/match-tile/index.tsx
+++ app/dashboard/match-tiles/match-tile/index.tsx
@@ -5,6 +5,7 @@ import ExpandableSection from './expandable-section';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
 import { Match } from '@/interfaces/match.interface';
+import { OddsValues } from '@/interfaces/odds.interface';
 import { LineItem } from '@/interfaces/ui.type';
 
 const MultiLineChart = dynamic(() => import('@/app/chart/multiline-chart'), {
@@ -23,6 +24,68 @@ const lineItems: LineItem[] = [
   { key: 'draw', label: 'Draw', color: 'gold' },
 ];
 
+const DUMMY_ODDS: OddsValues[] = [
+  {
+    home: 1.4,
+    away: 6.5,
+    draw: 5.5,
+    timestamp: new Date('2023-10-21T00:14:58.000Z'),
+  },
+  {
+    home: 1.73,
+    away: 5.25,
+    draw: 3.6,
+    timestamp: new Date('2023-10-21T03:17:58.000Z'),
+  },
+  {
+    home: 1.8,
+    away: 4.5,
+    draw: 3.6,
+    timestamp: new Date('2023-10-21T06:17:58.000Z'),
+  },
+  {
+    home: 2.3,
+    away: 3.2,
+    draw: 3.4,
+    timestamp: new Date('2023-10-21T09:17:58.000Z'),
+  },
+  {
+    home: 1.36,
+    away: 7,
+    draw: 5.75,
+    timestamp: new Date('2023-10-21T12:17:58.000Z'),
+  },
+  {
+    home: 1.44,
+    away: 7.5,
+    draw: 4.5,
+    timestamp: new Date('2023-10-21T15:17:58.000Z'),
+  },
+  {
+    home: 3.3,
+    away: 2.3,
+    draw: 3.2,
+    timestamp: new Date('2023-10-21T18:00:54.000Z'),
+  },
+  {
+    home: 7.5,
+    away: 1.36,
+    draw: 5.75,
+    timestamp: new Date('2023-10-21T21:13:29.000Z'),
+  },
+  {
+    home: 1.8,
+    away: 4.33,
+    draw: 3.75,
+    timestamp: new Date('2023-10-22T00:09:16.000Z'),
+  },
+  {
+    home: 1.5,
+    away: 5.75,
+    draw: 4.75,
+    timestamp: new Date('2023-10-22T03:03:04.000Z'),
+  },
+];
 const MatchTile = ({
   match,
   isExpanded = false,
@@ -69,8 +132,9 @@ const MatchTile = ({
 
       <ExpandableSection isExpanded={isExpanded}>
         <div className="u-p-4 u-bg-gray-800 u-text-white u-rounded u-overflow-auto sm:u-p-5 md:u-p-10">
+          {/* <MultiLineChart data={match.odds} xKey="timestamp" lineItems={lineItems} /> */}
           <MultiLineChart
-            data={match.odds}
+            data={DUMMY_ODDS}
             xKey="timestamp"
             lineItems={lineItems}
           />
