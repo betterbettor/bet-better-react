be0ff4e8708f895ca4bdb00f70e7ae69db1654cc
Louvre Chow <louvrecly@gmail.com>
2023-11-07 16:02:24 -0500

    feat/loading-spinner: add Spinner to the generic Loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,10 @@
+import Spinner from './spinner';
+
 const Loading = () => {
   return (
     <div className="u-p-3 u-w-full u-h-full u-text-yellow-400 u-text-xl u-text-center u-flex u-justify-center u-items-center">
-      Loading...
+      <Spinner className="u-mr-2" />
+      <span>Loading...</span>
     </div>
   );
 };
